b7c4dc0062ed6e61d2d9e5004cc3d7dde302fab0
wenxingW <48637953+wenxingW@users.noreply.github.com>
2023-05-01 18:47:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝讥（手动仙女符号）',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oL_Fw6UY0PEa9NLXf7gcHhWRkSPU',
+      id: 'oL_Fw6RDGob5MJYXwrWCOe-NXwUE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'q1rrDcubuxioYMCuJMao_QAW5mm_61B9A1FMf5qhbw0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oL_Fw6UY0PEa9NLXf7gcHhWRkSPU',
+      id: 'oL_Fw6RDGob5MJYXwrWCOe-NXwUE',
     }
   ],
 
