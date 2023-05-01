47f60faabe24abbb7518e81463a52f1c65faae89
wenxingW <48637953+wenxingW@users.noreply.github.com>
2023-05-01 18:29:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,13 +31,14 @@ const USER_CONFIG = {
         {
           type: '*生日', name: '宝讥阴历', year: '2000', date: '03-26',
         },
+         {
+          type: '节日', name: '我们的相恋纪念日', year: '2023', date: '04-25',
+        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '宝讥阳历', year: '2000', date: '04-30',
         },
-        {
-          type: '节日', name: '我们的相恋纪念日', year: '2023', date: '04-25',
-        },
+       
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
