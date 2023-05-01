17dca7bd83c829e39d046d64df211500bc55bdd4
wenxingW <48637953+wenxingW@users.noreply.github.com>
2023-05-01 18:13:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oL_Fw6UY0PEa9NLXf7gcHhWRkSPU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'vZz4e60rcMSnHQgSwItdmxVt4U8dwXC7L5zd6z2pWsw',
+      useTemplateId: 'L7lnkAgfG_qQHWbImmqVj2SEd-xMS2VBZu6qjsc1slo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-30',
       festivals: [
